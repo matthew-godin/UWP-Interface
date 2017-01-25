9eeba44d4a71c9de542865541d6111c32ac577e2
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2017-01-25 10:28:50 -0500

    Load Game menu created but not perfect.

--- UWPInterface/MainPage.xaml.cs
+++ UWPInterface/MainPage.xaml.cs
@@ -22,9 +22,79 @@ namespace UWPInterface
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        const int NUM_SAVES = 3, NUM_ROWS = 2;
+
+        Grid Columns { get; set; }
+        Button BackButton { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void LoadGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Children.Clear();
+            Display.RowDefinitions.Clear();
+            Display.ColumnDefinitions.Clear();
+            Columns = new Grid();
+            Columns.Children.Clear();
+            Columns.RowDefinitions.Clear();
+            Columns.ColumnDefinitions.Clear();
+            Button[] arrayOfSaveButtons = new Button[NUM_SAVES];
+            SetSaveButtons(arrayOfSaveButtons);
+            SetDisplayForLoadGame();
+        }
+
+        void SetDisplayForLoadGame()
+        {
+            RowDefinition firstRow = new RowDefinition();
+            firstRow.Height = new GridLength(10, GridUnitType.Star);
+            Display.RowDefinitions.Add(firstRow);
+            RowDefinition secondRow = new RowDefinition();
+            secondRow.Height = new GridLength(90, GridUnitType.Star);
+            Display.RowDefinitions.Add(secondRow);
+            BackButton = new Button();
+            BackButton.Margin = new Thickness(10, 10, 10, 10);
+            BackButton.Width = 300;
+            BackButton.Height = 50;
+            BackButton.Content = "Back";
+            BackButton.FontSize = 30;
+            BackButton.HorizontalAlignment = HorizontalAlignment.Left;
+            BackButton.VerticalAlignment = VerticalAlignment.Bottom;
+            BackButton.SetValue(Grid.RowProperty, 0);
+            Display.Children.Add(BackButton);
+            Columns.SetValue(Grid.RowProperty, 1);
+            Display.Children.Add(Columns);
+        }
+
+        void SetSaveButtons(Button[] buttons)
+        {
+            for (int i = 0; i < NUM_SAVES; ++i)
+            {
+                Columns.ColumnDefinitions.Add(new ColumnDefinition());
+                buttons[i] = new Button();
+                buttons[i].Margin = new Thickness(40, 0, 0, 40);
+                buttons[i].Width = 500;
+                buttons[i].Height = 800;
+                buttons[i].SetValue(Grid.ColumnProperty, i);
+                Columns.Children.Add(buttons[i]);
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Columns.Children.Clear();
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            Columns.Children.Clear();
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            Columns.Children.Clear();
         }
     }
 }
